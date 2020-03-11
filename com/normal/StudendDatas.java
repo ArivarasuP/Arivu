ead08aeeb0cad0aee6519ec00f73a5a9241488ac
ArivarasuP <arivarasu.arasu@gmail.com>
2020-03-11 10:10:19 +0530

    project git

---
+++ com/normal/StudendDatas.java
@@ -0,0 +1,17 @@
+package com.normal;
+
+public class StudendDatas {
+    public void stuName() {
+	System.out.println("thamizh");
+    }
+	public void studentId() {
+    System.out.println("thambaram");		
+	}
+
+	public static void main(String[]args) {
+		StudendDatas i = new StudendDatas();
+		i.stuName();
+		i.studentId();
+	}
+	
+}
